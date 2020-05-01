d9a636ca30c5cf8d938ed4fa9526a0b1ca66212a
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-05-02 00:14:31 +0530

    working process of C-lings that compiles two problems and prints message as per the compiltion status

---
+++ compile.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main ()
+	{
+		int i,j;
+    char k[64];
+    char x[64];
+    char y[64];
+    char gcc[512];
+      for(i=1; i<3; i++)
+        {
+            sprintf( x, "%d", i );
+            strcpy(k,"gcc ");
+            strcpy(y,".c");
+            strcat(k,x);
+            strcat(k,y);
+            sprintf(gcc,"%s",k);
+
+		       j=system(gcc);
+
+	
+              if(j==0)
+               {
+                printf("CONGRATULATIONS! %s is compiled\n",k);
+               }
+              else
+               {
+                printf("%s not compiled, for hint type <problem.hint>\n",k);
+               }
+        }
+        
+
+		return 0;
+	}
