c59effefe325e579b77e362d15c4e97f4a247ad7
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-05-04 00:16:01 +0530

    add hint for problem that isn't compiled

--- compile.c
+++ compile.c
@@ -1,35 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int main ()
-	{
-		int i,j;
-    char k[64];
-    char x[64];
-    char y[64];
-    char gcc[512];
-      for(i=1; i<3; i++)
-        {
-            sprintf( x, "%d", i );
-            strcpy(k,"gcc ");
-            strcpy(y,".c");
-            strcat(k,x);
-            strcat(k,y);
-            sprintf(gcc,"%s",k);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+  int i, j;
+  char k[64];
+  char x[64];
+  char y[64];
+  char h[64];
+  char hint[64];
+  char gcc[512];
+  for (i = 1; i < 4; i++)
+  {
+    sprintf(x, "%d", i);
+    strcpy(k, "gcc ~/C-lings/Problems/");
+    strcpy(y, ".c");
+    strcat(k, x);
+    strcat(k, y);
+    sprintf(gcc, "%s", k);
 
-		       j=system(gcc);
+    j = system(gcc);
 
-	
-              if(j==0)
-               {
-                printf("CONGRATULATIONS! %s is compiled\n",k);
-               }
-              else
-               {
-                printf("%s not compiled, for hint type <problem.hint>\n",k);
-               }
-        }
-        
+    if (j == 0)
+    {
+      printf("CONGRATULATIONS! %s is compiled\n", k);
+    }
+    else
+    {
+      printf("%s not compiled, \n solve error to compile\n for hint type <problem.hint>\n", k);
 
-		return 0;
-	}
+      strcpy(h, "problem.hint");
+      scanf("%s", hint);
+      if (strcmp(h, hint) == 0)
+      {
+        printf("try checking print statement\n");
+      }
+    }
+  }
+
+  return 0;
+}
