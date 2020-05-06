0670629a7443412507f5cbade2344f297e330e2c
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-05-06 21:17:19 +0530

    add path of the problems

--- compile.c
+++ compile.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
 int main()
 {
   int i, j;
@@ -10,16 +13,32 @@ int main()
   char h[64];
   char hint[64];
   char gcc[512];
+  char p[64];
+  char cwd[PATH_MAX];
+  if (getcwd(cwd, sizeof(cwd)) != NULL)
+  {
+    printf("Current working dir: %s\n", cwd);
+  }
+  else
+  {
+    perror("getcwd() error");
+    return 1;
+  }
   for (i = 1; i < 4; i++)
   {
     sprintf(x, "%d", i);
-    strcpy(k, "gcc ~/C-lings/Problems/");
+    strcpy(k, "gcc ");
+    strcat(k, cwd);
+    strcpy(p, "/Problems/");
+    strcat(k, p);
     strcpy(y, ".c");
     strcat(k, x);
     strcat(k, y);
+    printf("%s", k);
     sprintf(gcc, "%s", k);
 
     j = system(gcc);
+    printf("%d", j);
 
     if (j == 0)
     {
