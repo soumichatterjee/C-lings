e8d4d8e9811680d2f87eb4e7c48bf2fc36e1c755
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-05-17 23:00:02 +0530

    WIP

--- compile.c
+++ compile.c
@@ -4,58 +4,71 @@
 #include <unistd.h>
 #include <limits.h>
 
+char get_directory(char**);
+void compile();
 int main()
 {
-  int i, j;
-  char k[64];
-  char x[64];
-  char y[64];
-  char h[64];
-  char hint[64];
-  char gcc[512];
-  char p[64];
-  char cwd[PATH_MAX];
-  if (getcwd(cwd, sizeof(cwd)) != NULL)
-  {
-    printf("Current working dir: %s\n", cwd);
-  }
-  else
-  {
-    perror("getcwd() error");
-    return 1;
-  }
-  for (i = 1; i < 4; i++)
-  {
-    sprintf(x, "%d", i);
-    strcpy(k, "gcc ");
-    strcat(k, cwd);
-    strcpy(p, "/Problems/");
-    strcat(k, p);
-    strcpy(y, ".c");
-    strcat(k, x);
-    strcat(k, y);
-    printf("%s", k);
-    sprintf(gcc, "%s", k);
+    char **gcc[512];
+    get_directory(gcc);
+    
+}
+char get_directory(char** gcc)
+{
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        printf("%s\n", cwd);
+    }
+    else
+    {
+        perror("getcwd() error");
+    }
 
+    int i;
+    char x[64];
+    char k[64];
+    char p[64];
+    char y[64];
+    
+    
+    for (i = 1; i < 4; i++)
+    {
+        sprintf(x, "%d", i);
+        strcpy(k, "gcc ");
+        strcat(k, cwd);
+        strcpy(p, "/Problems/");
+        strcat(k, p);
+        strcpy(y, ".c");
+        strcat(k, x);
+        strcat(k, y);
+        sprintf(gcc, "%s", k);
+        printf("%s\n", gcc);
+    }
+    
+    return gcc;
+}
+void compile()
+{
+    char gcc[512];
+    char h[64];
+    char hint[64];
+    int j;
     j = system(gcc);
     printf("%d", j);
 
     if (j == 0)
     {
-      printf("CONGRATULATIONS! %s is compiled\n", k);
+        printf("CONGRATULATIONS!  is compiled\n");
     }
     else
     {
-      printf("%s not compiled, \n solve error to compile\n for hint type <problem.hint>\n", k);
+        printf(" not compiled, \n solve error to compile\n for hint type <problem.hint>\n");
 
-      strcpy(h, "problem.hint");
-      scanf("%s", hint);
-      if (strcmp(h, hint) == 0)
-      {
-        printf("try checking print statement\n");
-      }
+        strcpy(h, "problem.hint");
+        scanf("%s", hint);
+        if (strcmp(h, hint) == 0)
+        {
+            printf("try checking print statement\n");
+        }
     }
-  }
-
-  return 0;
 }
