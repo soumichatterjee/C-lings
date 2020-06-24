28a80e0edbfcc296dd48ca4087466e307b565c2b
Vishwaak <chandranvishwaak@gmail.com>
2020-06-25 02:16:13 +0530

    Compile: code cleanups
    Removed the redundant code and cleanup and Refactored.

--- compile.c
+++ compile.c
@@ -4,39 +4,43 @@
 #include <unistd.h>
 #include <limits.h>
 
+void get_directory(char *, int);
+void compile();
 int main()
 {
-  int i, j;
-  char k[64];
-  char x[64];
-  char y[64];
-  char h[64];
-  char hint[64];
-  char gcc[512];
-  char p[64];
-  char cwd[PATH_MAX];
-  if (getcwd(cwd, sizeof(cwd)) != NULL)
-  {
-  }
-  else
-  {
-    perror("getcwd() error");
-    return 1;
-  }
-  for (i = 1; i < 4; i++)
-  {
-    sprintf(x, "%d", i);
-    strcpy(k, "gcc ");
-    strcat(k, cwd);
-    strcpy(p, "/Problems/");
-    strcat(k, p);
-    strcpy(y, ".c");
-    strcat(k, x);
-    strcat(k, y);
-    printf("%s", k);
-    sprintf(gcc, "%s", k);
+    int i;
+    for (i = 1; i < 4; i++)
+    {
+        char *directory[512];
+        get_directory((char *)directory, i);
+        compile(directory);
+    }
+}
+void get_directory(char *path, int i)
+{
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        printf("problem-");
+    }
+    else
+    {
+        perror("getcwd() error");
+    }
 
-    j = system(gcc);
+        sprintf(path,"%s%s%d%s",cwd,"/Problems/",i,".c");
+        printf("%s\n", path);
+
+}
+void compile(char *paths[512])
+{
+    char h[64];
+    char hint[64];
+    char *gcc[512];
+    
+    sprintf(gcc,"%s%s","gcc ",paths);
+    int j = system(gcc);
+    printf("%d", j);
 
     if (j == 0)
     {
