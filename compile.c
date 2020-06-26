fc2fa170066d4348f6e94a281397aac56b22f916
Vishwaak <chandranvishwaak@gmail.com>
2020-06-26 20:19:41 +0530

    Compile: watch and verify commands
    Helps the users to compile the file if a change is detected
    and verify to check all the files.

--- compile.c
+++ compile.c
@@ -3,39 +3,94 @@
 #include <string.h>
 #include <unistd.h>
 #include <limits.h>
-
-void get_directory(char *, int);
+struct lists
+{
+    char array[50][100];
+};
+int exe_cnt =0; // Total number of excerises
+struct lists get_directory(char *);
 void compile();
 int Complete();
-int main()
+int main(int argc,char **argv)
 {
-    int i;
-    for (i = 1; i < 4; i++)
+    if(argc == 1)
     {
-        char *directory[512];
-        get_directory((char *)directory, i);
-        compile(directory);
-        if(Complete(directory) != 1)
-        {
-            printf("Remove //I AM NOT DONE comment to continue compilation \n");
-            break;
+        printf("Enter a valid argument verify/watch \n");
+        exit(-1);
+    }  
+    char *directory[512];
+    struct lists dir_list;
+    dir_list = get_directory((char *)directory); 
+ 
+    if(strcmp("watch",argv[1]) == 0)
+    {
+        int j=0;
+        while(j<exe_cnt)
+         {
+            sprintf(directory, dir_list.array[j]);
+            compile(directory);
+            if(Complete(directory) != 1)
+            {
+                printf("Remove //I AM NOT DONE comment to continue compilation \n");
+                char *u;
+                scanf("%c",u);
+            }
+            else j++;
+            
         }
     }
+    else if(strcmp("verify",argv[1]) == 0)
+    {
+        for(int j=1; j<exe_cnt; j++)
+        {
+            sprintf(directory, (char *)dir_list.array[j]);
+            compile(directory);
+        }
+    }
+    else
+    {
+        printf("Commnad not found \n");
+    }
+    
+    
 }
-void get_directory(char *path, int i)
+ struct lists get_directory(char *path)
 {
-    char cwd[PATH_MAX];
+    char cwd[PATH_MAX],command[128],*line =NULL;
+    size_t len = 0;
+    struct lists result;
     if (getcwd(cwd, sizeof(cwd)) != NULL)
     {
-        printf("problem-");
+        printf("problem- \n");
     }
     else
     {
         perror("getcwd() error");
     }
-
-        sprintf(path,"%s%s%d%s",cwd,"/Problems/",i,".c");
-        printf("%s\n", path);
+        strcat(command,"ls ");
+        strcat(command,cwd);
+        strcat(command,"/Problems/ > file.txt");
+        system(command);
+        FILE *fp;
+        if((fp = fopen("file.txt","r"))== NULL)
+        {
+            perror("Unable to Open file");
+            exit(1);
+        }
+        strcat(path,cwd);
+        strcat(path,"/Problems/");
+        int j=0;
+        while(getline(&line,&len,fp)!= -1){
+                strcat(result.array[j],path);
+                strcat(result.array[j],line);
+                printf("%s",result.array[j]);
+            
+            j++;       
+    }
+        exe_cnt =j;
+        return result;
+        //sprintf(path,"%s%s%d%s",cwd,"/Problems/",i,".c");
+        //printf("%s\n", path);
 
 }
 void compile(char *paths[512])
@@ -71,10 +126,9 @@ int Complete(char *files[512])
    char *file_name[512];
    FILE *fp;
    char *str = "//I AM NOT DONE";
-   
-   sprintf(file_name,"%s",files);
-
-   if((fp = fopen(file_name,"r")) == NULL)
+    sprintf(file_name,"%s",strtok(files,"\n"));
+    printf("%s \n",files);
+   if((fp = fopen(files,"r")) == NULL)
     {    
        perror("File Opening Error");
        return(-1);
