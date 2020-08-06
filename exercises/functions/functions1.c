226f57391ffcd1f3a19eb54b20067f865c5e9076
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-08-06 23:52:42 +0530

    problems based on functions

---
+++ exercises/functions/functions1.c
@@ -0,0 +1,29 @@
+// Based on their marks students will be given remarks by Mr.Ariel.
+// If any student has got atleast more than or equal to 90 in any of the subjects, then remark will be " WELL DONE!"
+// If marks are less than 90 then remark will be "Good,need to work hard! "
+// Use a function got90 to find out if the student has done well or needs to work hard.
+
+// For hint type "hint"
+
+#include <stdio.h>
+void got90(int[], int, int);
+int main()
+{
+    int marks[10], NumSub, i, cutoff = 90;
+    printf("Enter the number of subjects: \n");
+    scanf("%d\n", &NumSub);
+    printf("Enter marks: \n");
+    for (..........) //condition to scan the array
+    {
+        //scan the array
+    }
+}
+got90(marks, NumSub, cutoff)
+{
+
+    // Write a funtion to find out if the student has got 90 or above using a flag and break.
+
+    // Print messages accordingly
+}
+
+// I AM NOT DONE!
