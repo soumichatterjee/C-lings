1ebd95c9258da718afd930241715b3a47cfa359f
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-08-07 00:32:50 +0530

    problems based on loops

---
+++ exercises/loops/loop1.c
@@ -0,0 +1,18 @@
+// Students have got their numbers from the given values in the previous program. Now they want to calculate their aggregate.
+// Help them to do it using for loop.
+// For hint type "hint"
+#include <stdio.h>
+int main()
+{
+    int NumSub, marks, aggregate = 0;
+    printf("enter total number of subjects\n");
+    scanf("%d", &NumSub);
+    printf("enter marks in subjects:\n");
+    for (...........) //enter conditions
+
+    {
+        //scan individual marks and aggregate
+    }
+    printf("aggregate of marks is %d\n", aggregate);
+
+    //I AM NOT DONE!
