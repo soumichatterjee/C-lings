c1f04875b06281f76cb90891140fbeb77274de1d
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-08-06 23:42:30 +0530

    problems based on datatypes

---
+++ exercises/datatypes/datatypes2.c
@@ -0,0 +1,19 @@
+// Antonio lended money to Bassanio to visit Belmont with a condition that he will give proper details of his expenditure.
+// Bassanio is trying to print the actual expenditure i.e with deciamal but facing some issues.
+// Help him to do it.
+// For hint type "hint"
+
+#include <stdio.h>
+int main()
+{
+    int boatfare = 450.50;
+    int royalcloth = 8000.60;
+    int food = 600.20;
+    int gifts = 10000.00;
+    printf("money spent on travel = % \n",boatfare);
+    printf("money spent to buy clothes = %  \n",royalcloth);
+    printf("money spent on food = % \n",food);
+    printf("money spent on buying gifts = % \n ",gifts);
+    return 0;
+}
+//I AM NOT DONE!
