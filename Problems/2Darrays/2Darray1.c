c0b4fa796faaa3d558aa0c24affc8cfc1a72ffc1
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-08-09 23:40:35 +0530

    problems based on 2D arrays

---
+++ Problems/2Darrays/2Darray1.c
@@ -0,0 +1,64 @@
+// Salerio, Gratiano and Lorenzo have raised fund for la Befana. Their collection on day 1 is {425, 343, 123} Euro respectively,
+// On day 2 {345,656,432} Euro respectively.
+// On day 3 {534,265,453} Euro respectively.
+// Write a program to find out the day of maximum collection and maximum collection by the person by using arrays.
+
+#include
+
+int main()
+{
+	int days, person, row, col, sum = 0, row_index = 0, column_index = 0;
+	scanf(“%d %d”,&days,&person);
+	int row_arr[days];
+	int i, j;
+	int mat[days][person];
+	for(................)
+		{
+			for(...................)
+				scanf(“%d”,&mat[i][j]);
+		}
+
+	int z = 0;
+
+	printf(“Sum of rows is “);
+	for(..........................)
+		{
+			sum = 0;
+				for(.....................)
+					{
+						sum += mat[row][col];
+					}
+			printf(“%d “,sum);
+			row_arr[z++] = sum;
+		}
+	int temp_row = row_arr[0];
+	for(i=1;i<days;i++)
+		{
+			if(temp_row < row_arr[i])
+				{
+					temp_row = row_arr[i];
+					row_index = i;
+				}
+}
+	printf(“\n day %d has maximum sum “, row_index + 1);
+
+	printf(“\nSum of columns is “);
+	sum = 0;
+	int y = 0;
+	int col_arr[person];
+	for (i = 0; i < person; ++i)
+		
+
+        //write code
+
+
+	int temp_col = col_arr[0];
+	for(i=1;i<person;i++)
+		
+
+        //write code
+
+
+	printf(“\n person %d has maximum sum “, column_index + 1);
+	return 0;
+}
---
+++ Problems/2Darrays/2Darray3.c
@@ -0,0 +1,46 @@
+// Write a program to make upper triangle of a mtrix '*' and lower triangle '#'.
+
+
+
+#include <stdio.h>
+
+void main()
+{
+    int matrix[10][10], i, j, n;
+    float d=0;
+
+
+    printf("Input the size of the square matrix : ");
+    scanf("%d", &n);
+    printf("Input elements in the first matrix :\n");
+
+
+    //code
+
+
+    printf("The matrix :\n");
+    for (i=0;i<n;i++)
+    {
+        for (j=0;j<n;j++)
+            printf("% 4d", matrix[i][j]);
+        printf("\n");
+    }
+
+    printf("\nSetting * in upper triangular matrix\n");
+    for (.............)
+    {
+
+
+        //code
+
+
+    }
+
+    printf("\nSetting # in lower triangular matrix\n");
+    for (...........) {
+
+
+        //code
+
+
+    }
