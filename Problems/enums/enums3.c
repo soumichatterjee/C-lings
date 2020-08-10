2a02edae4b8e4a9bd94d00ecd205fc6e467297ac
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-08-11 01:36:59 +0530

    problems based on enumerators

---
+++ Problems/enums/enums3.c
@@ -0,0 +1,16 @@
+// Solve error
+// For hint type "hint"
+
+#include<stdio.h>
+
+enum race1{ onyourmark, set};
+
+enum race2{ set, go};
+
+int main(){
+    printf("set at %d ", set);
+
+    return 0;
+}
+
+// I AM NOT DONE!
