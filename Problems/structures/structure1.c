417fe060774573420e6cc1a588d0c5b36e360064
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-08-11 01:39:44 +0530

    problems based on structures

---
+++ Problems/structures/structure1.c
@@ -0,0 +1,30 @@
+// Mr. S Singh wants to declare the names of house captains for session 2020-2021. Before that he wants to store detailed information of each captain.
+// The data that he wants to keep account of is housename, captainname, class, roll.
+// Help him to do it using structures.
+//For hints type "hint"
+
+
+
+#include<stdio.h>
+// Declare the Structure
+struct captain
+
+{
+    
+    // Declare the struct
+
+};
+int main()
+
+{
+
+    struct captain red, yellow, blue, green;
+
+    // mention the required data
+
+
+printf(............................);
+return 0;
+}
+
+// I AM NOT DONE!
