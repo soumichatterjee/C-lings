1b79962a11c5ad6adca5059689d16694f891866b
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-08-11 01:35:23 +0530

    problem based on function

---
+++ Problems/functions/function2.c
@@ -0,0 +1,20 @@
+// Write a program to print a series of number starting from 0 in which a number corresponds to sum of previous two nunmbers.
+// Use function.
+// For hint type "hint"
+
+int specialseries(int i) {
+
+   // code
+
+int  main() {
+
+   int i;
+	
+   for (i = 0; i < 10; i++) {
+      printf("%d\t\n", specialseries(i));
+   }
+	
+   return 0;
+}
+
+// I AM NOT DONE!
