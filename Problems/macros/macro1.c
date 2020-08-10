58c519df83ba1ffc19d59415393d27c81615eb04
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-08-11 01:38:22 +0530

    problems based on macros

---
+++ Problems/macros/macro1.c
@@ -0,0 +1,17 @@
+// Using macro swap two numbers using the given parameters.
+// For hint typr "hint"
+
+
+#include<stdio.h>
+
+// declare macro
+
+int main()
+{
+    int num1 = 5668484653156, num2 = 34978643651848;
+    SWAP(num1, num2, int);
+    printf("%d %d\n", num1, num2);
+    return 0;
+}
+
+// I AM NOT DONE!
---
+++ Problems/macros/macro2.c
@@ -0,0 +1,22 @@
+// Mili has drawn a closed structures consisting of three sides. Now she wants to calculate its area using macro.
+// Help her to do it.
+// For hint type "hint"
+
+#include<stdio.h>
+
+// define macro
+
+int main()
+{
+	float base, height, a;
+	printf("enter base of triangle: ");
+	scanf("%f",&base);
+	
+	printf("enter height of the triangle: ");
+	scanf("%f",&height);
+        a=area(b,h); 
+	printf("Area of the triangle : %f\n",a);
+	return 0;
+}
+
+// I AM NOT DONE!
