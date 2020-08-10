4a2cdd0f63c4b507fb10792fd47c8b11b3f38b93
Chatterjee-31 <soumichatterjee3108@gmail.com>
2020-08-11 01:36:59 +0530

    problems based on enumerators

---
+++ Problems/enums/enum1.c
@@ -0,0 +1,19 @@
+// Form a enum of trangenic plants namely Bt cotton, wheat, brassica napus, golden rice, Flavr Savr Tomato and print their values.
+// Initialise the value to 1
+// For hints type "hint"
+
+#include<stdio.h>
+
+// declare enum
+
+int main (){
+
+    enum trans_plants plant;
+
+
+    // print values
+
+    return 0;
+}
+
+// I AM NOT DONE!
---
+++ Problems/enums/enum2.c
@@ -0,0 +1,41 @@
+// Ferdinand is a teacher at a kindergarten school.
+// He wants to teach his students the name of the month and the number at which they occur, help him do that using switch and enumerator.
+// For hints type "hint"
+
+
+#include<stdio.h>
+// declare the enum months
+int main()
+{
+    enum months number;
+    int n;
+    printf("enter no. 1-12\n");
+    scanf("%d",&n);
+    switch(n)
+    {
+        
+    case 1:
+        printf("January");
+        break;
+    .
+    .
+    .
+    .
+    .
+    .
+    .   // write for each month
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+
+    default :
+        printf("invalid number!!!");
+    }
+}
+
+// I AM NOT DONE!
